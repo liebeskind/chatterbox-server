05d1e8d4de28e1b8dc250112478a428e709aa624
Daniel <daniel.liebeskind@gmail.com>
2013-10-15 17:09:17 -0700

    Angular html added and provides standalone client

--- server/html-engine.js
+++ server/html-engine.js
@@ -1,46 +1,7 @@
 
-var render = function(request, response, messages){
-
-  var body = '<html ng-app>'+
-            '<head>'+
-              '<title>Your Shopping Cart</title>'+
-            '</head>'+
-            '<body >'+
-              '<h1>Chatterbox</h1>'+
-              '<form ng-controller="ChatFormController">'+
-                'Message: <input ng-model="msg">'+
-                '<button ng-click="POSTmessage()">Submit Message</button>'+
-
-              '</form>'+
-              '<div ng-controller="CartController">'+
-                '<div ng-repeat="item in items">'+
-                  '<span>{{item.username + ": "}}</span>'+
-                  '<span>{{item.message}} </span>'+
-                  //'<button ng-click="remove($index)">Remove message</button>'+
-                '</div>'+
-              '</div>'+
-              '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>'+
-              '<script>'+
-              'function ChatFormController($scope,$http){'+
-                '$scope.POSTmessage = function(){'+
-                  'var msg = {text: $scope.msg};'+
-                  'msg = JSON.stringify(msg);'+
-                  '$http.post("http://localhost:8080",msg);'+
-                '}'+
-              '}'+
-              'function CartController($scope, $http) {'+
-                'console.log($http.get("http://localhost:8080/"));'+
-                '$scope.items = JSON.stringify($http.get("http://localhost:8080/"));'+ //JSON.stringify(messages)
-                '$scope.remove = function(index) {'+
-                  '$scope.items.splice(index, 1);'+
-                '}'+
-              '}'+
-              '</script>'+
-            '</body>'+
-            '</html>'+
-
-  response.writeHead(200,  {"Content-Type": "text/html"});
-  response.write(body);
+var render = function(request, response, messages,headers){
+  response.writeHead(200, headers);
+  // response.write(body);
   response.end();
 };
 
--- server/request-handler.js
+++ server/request-handler.js
@@ -4,11 +4,11 @@ var html = require('./html-engine');
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
+  "access-control-allow-headers": "content-type, accept, X-Requested-With",
   "access-control-max-age": 10 // Seconds.
 };
 var headers = defaultCorsHeaders;
-headers['Content-Type'] = "text/plain";
+headers['Content-Type'] = "application/json";
 
 
 var handleRequest = function(request, response) {
@@ -22,8 +22,8 @@ var handleRequest = function(request, response) {
 
   if(request.method === 'GET'){//&& pathname === '/'){ //1/classes/chatterbox'){
     response.writeHead(200,headers);
-    html.render(request,response,messages);
-    // response.end(JSON.stringify({results:messages}));
+    //html.render(request,response,messages,headers);
+    response.end(JSON.stringify(messages));
   } else if(request.method === 'GET'){
     response.writeHead(404,headers);
     response.end();
@@ -40,13 +40,14 @@ var handleRequest = function(request, response) {
   }
 
   var addMessage = function(request){
-    // console.log(request);
+    console.log(request);
     data = JSON.parse(request);
+    console.log('LOADING MESSAGE   ', data.message);
     messages.push({
       createdAt: new Date(),
       roomname: data.roomname,
       username: data.username,
-      text: data.text
+      message: data.message
     });
     response.writeHead(201,headers);
     response.end();
