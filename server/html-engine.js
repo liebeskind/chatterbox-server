237fade45561d7679709b7d2a92b64054fad0cc6
Daniel <daniel.liebeskind@gmail.com>
2013-10-15 19:09:31 -0700

    Tweaked package.json, removed html-engine.js

--- server/html-engine.js
+++
@@ -1,8 +0,0 @@
-
-var render = function(request, response, messages,headers){
-  response.writeHead(200, headers);
-  // response.write(body);
-  response.end();
-};
-
-module.exports.render = render;
